24801e0856695655574462b6fcaeda2c02af6de0
👑 𝐌𝐑 𝐑𝐈𝐂𝐎 👑 <max2005alg@gmail.com>
2025-07-24 21:20:27 +0100

    Delete kevin1.py

--- kevin1.py
+++
@@ -1,9 +0,0 @@
-
-# ملف تشغيل للوحدة المشفرة kevin1.cpython-312.so
-import kevin1
-
-# تأكد أن الدالة الرئيسية موجودة داخل الملف الأصلي
-if hasattr(kevin1, 'main'):
-    kevin1.main()
-else:
-    print("تم استيراد kevin1 بنجاح، ولكن لا توجد دالة main للتشغيل.")
